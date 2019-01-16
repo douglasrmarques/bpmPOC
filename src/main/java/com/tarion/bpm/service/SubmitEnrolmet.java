cb73a220240ba580794c29d756f5669a06bc3385
douglasrmarques <douglasreppr@gmail.com>
2019-01-16 12:42:04 -0500

    changes over coding

--- src/main/java/com/tarion/bpm/service/SubmitEnrolmet.java
+++ src/main/java/com/tarion/bpm/service/SubmitEnrolmet.java
@@ -12,17 +12,6 @@ public class SubmitEnrolmet implements ISubmitEnrolmet {
 	
 	@Override
 	public void submitEnrolment() {
-//		MunicipalAddress municipalAddress = new MunicipalAddress();
-//		municipalAddress.setCity("Toronto");
-//		municipalAddress.setPostalCode("M2J0B8");
-//		municipalAddress.setStreetName("130 PARKWAY FOREST");
-//		municipalAddress.setStreetNo("130");
-//		municipalAddress.setSuiteNo("202");
-//		
-//		ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("registerEnrolment");
-//		ObjectValue typedCustomerValue = Variables.objectValue(municipalAddress).serializationDataFormat("application/json").create();
-//		runtimeService.setVariable(processInstance.getId(), "customer", typedCustomerValue);
-		
 		runtimeService.startProcessInstanceByKey("registerEnrolment");
 	}
 
